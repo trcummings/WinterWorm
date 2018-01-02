bc93f5dc0f8c714b7bdb7632bcc26a2b913d591b
Thomsen Cummings <thomsencummings@gmail.com>
2018-01-02 02:34:37 -0500

    remove old loop files

--- game/lib/types.js
+++
@@ -1,9 +0,0 @@
-// @flow
-
-// Dt
-export opaque type Dt: number = number;
-
-export const toDt = (
-  timestamp: DOMHighResTimeStamp | number = 0,
-  startTime: number = 0
-): Dt => timestamp - startTime;
