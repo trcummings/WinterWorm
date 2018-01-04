f300146adbac6b9a835f2745fcd8f00152660775
Thomsen Cummings <thomsencummings@gmail.com>
2018-01-04 00:35:42 -0500

    more file deletion

--- game/engine/systems/events.js
+++
@@ -1,9 +0,0 @@
-import { assocPath } from 'ramda';
-
-import { queuePath, clearEventsQueue } from '../events';
-
-// Adds an events entry to the state object.
-export const initEventsSystem = gameState => assocPath(queuePath, {}, gameState);
-
-// Clear out events queue. Returns update game state.
-export const eventSystem = gameState => clearEventsQueue(gameState);
