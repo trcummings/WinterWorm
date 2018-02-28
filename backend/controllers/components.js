22cd0c64739291e6b8d4cd038763e1440b507b80
Thomsen Cummings <thomsencummings@gmail.com>
2018-02-28 16:35:35 -0500

    use models instead

--- backend/controllers/components.js
+++ backend/controllers/components.js
@@ -1,16 +1,14 @@
 const app = require('../server');
-const { db, models } = require('../models');
+const { models } = require('../models');
 
 // Components Controller
 app.get('/components', (req, res) => {
-  db.query('SELECT * FROM components')
-    .then(rows => res.send(JSON.stringify(rows)));
+  models.Components.findAll().then(rows => res.send(JSON.stringify(rows)));
 });
 
 app.get('/components/:id', (req, res) => {
   const id = req.params.id;
-  db.query(`SELECT * FROM systems WHERE id = ${id}`)
-    .then(rows => res.send(JSON.stringify(rows)));
+  models.Components.findById(id).then(rows => res.send(JSON.stringify(rows)));
 });
 
 app.post('/components', (req, res) => {
